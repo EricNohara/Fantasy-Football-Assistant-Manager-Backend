247c9ebbaa13f631bc86b5bd23977f0e33c09000
EricNohara <ernohara@bu.edu>
2025-11-06 16:55:29 -0500

    Add daily update route

--- Fantasy-Football-Assistant-Manager/Controllers/AutoUpdateController.cs
+++ Fantasy-Football-Assistant-Manager/Controllers/AutoUpdateController.cs
@@ -1,31 +1,28 @@
 ﻿using Fantasy_Football_Assistant_Manager.Services;
-using Fantasy_Football_Assistant_Manager.Models.Supabase;
-using Fantasy_Football_Assistant_Manager.Utils;
 using Microsoft.AspNetCore.Mvc;
 using Supabase;
-using Microsoft.AspNetCore.Razor.TagHelpers;
 
 
 namespace Fantasy_Football_Assistant_Manager.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class WeeklyUpdateController: ControllerBase
+public class AutoUpdateController: ControllerBase
 {
     private readonly NflVerseService _nflVerseService;
     private readonly Client _supabase;
     private readonly UpdateSupabaseService _updateSupabaseService;
 
-    public WeeklyUpdateController(NflVerseService nflVerseService, Client supabase)
+    public AutoUpdateController(NflVerseService nflVerseService, Client supabase)
     {
         _nflVerseService = nflVerseService;
         _supabase = supabase;
         _updateSupabaseService = new UpdateSupabaseService(_nflVerseService, _supabase);
     }
 
-    // PUT route which updates all relevant databases
-    [HttpPut]
-    public async Task<IActionResult> Put()
+    // PUT route for weekly updates
+    [HttpPut("weekly")]
+    public async Task<IActionResult> PutWeekly()
     {
         try
         {
@@ -40,6 +37,26 @@ public class WeeklyUpdateController: ControllerBase
 
             // update the team season stats
             await _updateSupabaseService.UpdateAllTeamSeasonStatsAsync();
+
+            // update the games this week (delete and add new games)
+            await _updateSupabaseService.UpdateGamesThisWeekAsync();
+
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, $"Error performing the weekly update: {ex.Message}");
+        }
+    }
+
+    // PUT route for daily updates (player injury status, game odds)
+    [HttpPut("daily")]
+    public async Task<IActionResult> PutDaily()
+    {
+        try
+        {
+            // update the non stat data for players (e.g. status)
+            await _updateSupabaseService.UpdateAllPlayerNonStatDataAsync();
 
             // update the games this week (delete and add new games)
             await _updateSupabaseService.UpdateGamesThisWeekAsync();
