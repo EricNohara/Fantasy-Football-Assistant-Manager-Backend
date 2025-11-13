324e6bcc3898c5c529047ffd0cb7cbed11e247c2
NoahP-K <noahpk@bu.edu>
2025-11-13 14:44:49 -0500

    Bug fix in UserTeamMember

--- Fantasy-Football-Assistant-Manager/DTOs/UserTeamMember.cs
+++ Fantasy-Football-Assistant-Manager/DTOs/UserTeamMember.cs
@@ -5,7 +5,7 @@ namespace Fantasy_Football_Assistant_Manager.DTOs
     public class UserTeamMember
     {
         [JsonPropertyName("picked")]
-        public Boolean Picked = false;
+        public bool Picked { get; set; } = false;
 
         public required DTOs.PlayerWithStats Player { get; set; }
     }
