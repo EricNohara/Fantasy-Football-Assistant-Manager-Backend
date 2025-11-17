e97fc295d17c061565bcb4e555379c13594651a7
EricNohara <ernohara@bu.edu>
2025-11-17 13:56:32 -0500

    Add update to user sign up route and restructured program.cs for intuitiveness

--- FFOracle/DTOs/CreateUserRequest.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace FFOracle.DTOs;
-
-public class CreateUserRequest
-{
-    public required string Email { get; set; }
-    public required string Password { get; set; }
-    public string? TeamName { get; set; }
-}
---
+++ FFOracle/DTOs/Requests/CreateUserRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace FFOracle.DTOs.Requests;
+
+public class SignUpRequest
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string? Fullname { get; set; }
+    public string? PhoneNumber { get; set; }
+    public bool AllowEmails { get; set; }
+}
