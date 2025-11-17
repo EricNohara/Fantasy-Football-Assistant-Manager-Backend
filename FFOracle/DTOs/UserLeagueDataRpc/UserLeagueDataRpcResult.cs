ee013724a5c47e7895c8e0d7adb95b1b1dede276
EricNohara <ernohara@bu.edu>
2025-11-16 19:11:34 -0500

    Add change to retrieve opponent data for defensive teams too

--- FFOracle/DTOs/UserLeagueDataRpc/UserLeagueDataRpcResult.cs
+++ FFOracle/DTOs/UserLeagueDataRpc/UserLeagueDataRpcResult.cs
@@ -204,6 +204,9 @@ public class DefenseDto
 
     [JsonPropertyName("seasonStats")]
     public DefenseSeasonStatsDto SeasonStats { get; set; }
+
+    [JsonPropertyName("opponent")]
+    public DefenseOpponentDto Opponent { get; set; }
 }
 
 public class DefenseTeamInfoDto
@@ -226,4 +229,36 @@ public class DefenseSeasonStatsDto
     [JsonPropertyName("def_tds")] public int? DefensiveTouchdowns { get; set; }
     [JsonPropertyName("safeties")] public int? Safeties { get; set; }
     [JsonPropertyName("pass_defended")] public int? PassDefended { get; set; }
+}
+
+public class DefenseOpponentDto
+{
+    [JsonPropertyName("team")]
+    public OpponentTeamDto Team { get; set; }
+
+    [JsonPropertyName("offensiveStats")]
+    public TeamOffensiveStatsDto OffensiveStats { get; set; }
+}
+
+public class TeamOffensiveStatsDto
+{
+    // Passing
+    [JsonPropertyName("completions")] public int? Completions { get; set; }
+    [JsonPropertyName("passing_attempts")] public int? PassingAttempts { get; set; }
+    [JsonPropertyName("passing_yards")] public int? PassingYards { get; set; }
+    [JsonPropertyName("passing_tds")] public int? PassingTds { get; set; }
+    [JsonPropertyName("passing_interceptions")] public int? PassingInterceptions { get; set; }
+    [JsonPropertyName("sacks_against")] public int? SacksAgainst { get; set; }
+    [JsonPropertyName("fumbles_against")] public int? FumblesAgainst { get; set; }
+
+    // Rushing
+    [JsonPropertyName("carries")] public int? Carries { get; set; }
+    [JsonPropertyName("rushing_yards")] public int? RushingYards { get; set; }
+    [JsonPropertyName("rushing_tds")] public int? RushingTds { get; set; }
+
+    // Receiving
+    [JsonPropertyName("receptions")] public int? Receptions { get; set; }
+    [JsonPropertyName("targets")] public int? Targets { get; set; }
+    [JsonPropertyName("receiving_yards")] public int? ReceivingYards { get; set; }
+    [JsonPropertyName("receiving_tds")] public int? ReceivingTds { get; set; }
 }
