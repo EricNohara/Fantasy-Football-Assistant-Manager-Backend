af4760ceeac406525a9251a266c96f8863fc5f8f
EricNohara <ernohara@bu.edu>
2025-11-22 23:56:28 -0600

    Add code for updating a user

--- FFOracle/DTOs/Requests/UserRequest.cs
+++ FFOracle/DTOs/Requests/UserRequest.cs
@@ -8,3 +8,11 @@ public class SignUpRequest
     public string? PhoneNumber { get; set; }
     public bool AllowEmails { get; set; }
 }
+
+public class UpdateUserRequest
+{
+    public string? Fullname { get; set; }
+    public string? PhoneNumber { get; set; }
+    public bool AllowEmails { get; set; } = true;
+    public int TokensLeft { get; set; } = 0;
+}
