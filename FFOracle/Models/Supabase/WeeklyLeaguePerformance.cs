349c66428448d7b686d2737fb8526fc71b06a20c
EricNohara <ernohara@bu.edu>
2025-11-24 20:42:30 -0600

    Add models for new performance tables

---
+++ FFOracle/Models/Supabase/WeeklyLeaguePerformance.cs
@@ -0,0 +1,32 @@
+﻿using Supabase.Postgrest.Models;
+using Supabase.Postgrest.Attributes;
+using System.Text.Json.Serialization;
+
+namespace FFOracle.Models.Supabase;
+
+[Table("weekly_league_performance")]
+public class WeeklyLeaguePerformance : BaseModel
+{
+
+    [PrimaryKey("league_id")]
+    [Column("league_id")]
+    [JsonPropertyName("league_id")]
+    public Guid LeagueId { get; set; }
+
+    [PrimaryKey("week")]
+    [Column("week")]
+    [JsonPropertyName("week")]
+    public int Week { get; set; }
+
+    [Column("actual_fpts")]
+    [JsonPropertyName("actual_fpts")]
+    public double ActualFpts { get; set; }
+
+    [Column("max_fpts")]
+    [JsonPropertyName("max_fpts")]
+    public double MaxFpts { get; set; }
+
+    [Column("accuracy")]
+    [JsonPropertyName("accuracy")]
+    public double Accuracy { get; set; }
+}
---
+++ FFOracle/Models/Supabase/WeeklyLeaguePlayerPerformance.cs
@@ -0,0 +1,37 @@
+﻿using Supabase.Postgrest.Models;
+using Supabase.Postgrest.Attributes;
+using System.Text.Json.Serialization;
+
+namespace FFOracle.Models.Supabase;
+
+[Table("weekly_league_player_performance")]
+public class WeeklyLeaguePlayerPerformance: BaseModel
+{
+
+    [PrimaryKey("league_id")]
+    [Column("league_id")]
+    [JsonPropertyName("league_id")]
+    public Guid LeagueId { get; set; }
+
+    [PrimaryKey("week")]
+    [Column("week")]
+    [JsonPropertyName("week")]
+    public int Week { get; set; }
+
+    [PrimaryKey("player_id")]
+    [Column("player_id")]
+    [JsonPropertyName("player_id")]
+    public String PlayerId { get; set; }
+
+    [Column("actual_fpts")]
+    [JsonPropertyName("actual_fpts")]
+    public double ActualFpts { get; set; }
+
+    [Column("max_fpts")]
+    [JsonPropertyName("max_fpts")]
+    public double MaxFpts { get; set; }
+
+    [Column("was_correct")]
+    [JsonPropertyName("was_correct")]
+    public bool WasCorrect { get; set; }
+}
